123e1e420dc1943cf2f70540c2eafbd578158b3a
aarkitekkt <johnson.brycej@gmail.com>
2020-04-23 21:08:39 -0600

    built user page

--- client/src/components/SearchForm/index.js
+++ client/src/components/SearchForm/index.js
@@ -2,7 +2,12 @@ import React from "react";
 
 function SearchForm() {
     return (
-        <h1>SearchForm</h1>
+        <form className="form-inline">
+            <input type="text" className="form-control" id="locationInput" placeholder="location"></input>
+            <input type="text" className="form-control" id="magnitudeInput" placeholder="magnitude"></input>
+            <input type="number" className="form-control" id="proximityInput" placeholder="proximity (km)"></input>
+            <button type="submit" className="btn">Submit</button>
+        </form>
     )
 }
 
