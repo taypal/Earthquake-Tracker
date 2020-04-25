3ce43ccd49c38f1388345e124fceb0e7161f2242
Garrett Howard <hylander.garrett@gmail.com>
2020-04-25 12:19:29 -0600

    reconfigured Auth0 to restrict access to dashboard unless the user is signed in. sign-in/sign-out button now correctly logs user out

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,43 @@
-import React from 'react';
-import { Route } from 'react-router-dom';
+import React, { Component } from 'react';
+import { Route, withRouter } from 'react-router-dom';
+import auth0Client from './Auth';
 import Callback from './Callback';
 import Home from '../src/pages/Home';
-import SignIn from '../src/pages/Signin';
 import User from '../src/pages/User';
+import SecuredRoute from './SecuredRoute';
 
 
-function App() {
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      checkingSession: true,
+    }
+  }
 
-  return (
-    <div className="App" >
-      <Route exact path='/' component={Home} exact />
-      <Route exact path='/callback' component={Callback} exact />
-      <Route exact path='/signin' component={SignIn} exact />
-      <Route exact path='/dashboard' component={User} exact />
-    </div>
-  )
+  async componentDidMount() {
+    if (this.props.location.pathname === '/callback') {
+      this.setState({ checkingSession: false });
+      return;
+    }
+    try {
+      await auth0Client.silentAuth();
+      this.forceUpdate();
+    } catch (err) {
+      if (err.error !== 'login_required') console.log(err.error);
+    }
+    this.setState({ checkingSession: false });
+  }
+
+  render() {
+    return (
+      <div className="App" >
+        <Route exact path='/' component={Home} exact />
+        <SecuredRoute path='/dashboard' component={User} checkingSession={this.state.checkingSession} />
+        <Route exact path='/callback' component={Callback} exact />
+      </div>
+    )
+  }
 }
 
-export default App;
+export default withRouter(App);
--- client/src/components/Navbar/index.js
+++ client/src/components/Navbar/index.js
@@ -1,8 +1,15 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, withRouter } from "react-router-dom";
+import auth0Client from "../../Auth"
 import "./style.css";
 
-function Navbar() {
+function Navbar(props) {
+
+    const signOut = () => {
+        auth0Client.signOut();
+        props.history.replace('/');
+    };
+
     return (
         <div id="nav" className="container-fluid bg-dark">
             <div className="container">
@@ -15,11 +22,20 @@ function Navbar() {
                     <div className="d-flex col-12 col-md-8 justify-content-center justify-content-md-end">
                         <ul className="nav pb-3 pt-3">
                             <li className="nav-item">
-                                <Link to="./dashboard" className="px-3 align-middle text-white">dashboard</Link>
+                                <Link to="./dashboard" className="px-3 align-middle text-white">Dashboard</Link>
                             </li>
-                            <li className="nav-item">
-                                <a href="/signin" className="px-3 align-middle text-white">Sign-In</a>
-                            </li>
+                            {
+                                !auth0Client.isAuthenticated() &&
+                                <li className="nav-item">
+                                    <a className="px-3 align-middle text-white" onClick={auth0Client.signIn}>Sign In</a>
+                                </li>
+                            }
+                            {
+                                auth0Client.isAuthenticated() &&
+                                <li className="nav-item">
+                                    <a className="px-3 align-middle text-white" onClick={() => { signOut() }}>Sign Out</a>
+                                </li>
+                            }
                         </ul>
                     </div>
                 </div>
@@ -28,4 +44,4 @@ function Navbar() {
     )
 };
 
-export default Navbar;
+export default withRouter(Navbar);
