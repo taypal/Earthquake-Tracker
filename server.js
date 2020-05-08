65d6062f69ece7c1d9b2dfbe7a424ebd0456e1db
Garrett Howard <hylander.garrett@gmail.com>
2020-05-08 17:34:10 -0600

    heroku

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.use(express.json());
 
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
-    app.use(express.static("client/build"));
+    app.use(express.static(path.join(__dirname, 'client/build')));
 }
 
 // Define API routes here
@@ -23,9 +23,9 @@ app.use(routes);
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/earthquakedb");
 
 // Send every other request to the React app
-app.get("/*", (req, res) => {
-    res.sendFile(path.join(__dirname, 'build', 'index.html'));
-});
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname + '/client/build/index.html'));
+});;
 
 app.listen(PORT, () => {
     console.log(`🌎 ==> API server now on port ${PORT}!`);
