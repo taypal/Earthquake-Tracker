c13b98d1d96b4924cbe5aff4f518acfc8b384a9c
Garrett Howard <hylander.garrett@gmail.com>
2020-05-09 00:24:37 -0600

    static json

--- server.js
+++ server.js
@@ -24,9 +24,9 @@ app.use(routes);
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/earthquakedb");
 
 // Send every other request to the React app
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname + '/client/build/index.html'));
-});;
+// app.get('/*', (req, res) => {
+//     res.sendFile(path.join(__dirname + '/client/build/index.html'));
+// });;
 
 app.listen(PORT, () => {
     console.log(`🌎 ==> API server now on port ${PORT}!`);
