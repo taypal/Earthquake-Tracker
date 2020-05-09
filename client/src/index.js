23921aa87d7158e762200b27a7162a87953585bf
Garrett Howard <hylander.garrett@gmail.com>
2020-05-08 22:43:53 -0600

    trying to change to hash router for heroku

--- client/src/index.js
+++ client/src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import * as serviceWorker from './serviceWorker';
-import { BrowserRouter, HashRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 
 ReactDOM.render(
     <HashRouter>
@@ -13,3 +13,4 @@ ReactDOM.render(
 
 serviceWorker.unregister();
 
+//hash router instead of browserRouter
